1bef635119deedd62f7dcd20c2af4dbc10d30a73
Eric Hernandez <eands9@yahoo.com>
2019-01-13 06:14:13 -0600

    latin-23

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,10 +12,10 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"femininity", sentence:""))
-        list.append(Word(word:"confidence", sentence:""))
-        list.append(Word(word:"aggregate", sentence:""))
-        list.append(Word(word:"popularity", sentence:""))
-        list.append(Word(word:"diary", sentence:""))
+        list.append(Word(word:"humble", sentence:""))
+        list.append(Word(word:"vivisection", sentence:""))
+        list.append(Word(word:"strict", sentence:""))
+        list.append(Word(word:"prosecute", sentence:""))
+        list.append(Word(word:"contiguous", sentence:""))
     }
 }
